b4d3441f22f2d132209a1032464c3eb70ad5a676
ClaudiaBiplus <claudia.filannino@gmail.com>
2023-11-07 11:03:15 +0000

    added questions

---
+++ assets/js/question.js
@@ -0,0 +1,108 @@
+const questions = [
+    {
+      title: "What does HTML stand for?",
+      choices: [
+        "Hyper Trainer Marking Language",
+        "Hyper Text Markup Language",
+        "Hyper Texts Market Language",
+        "Hyper Text Markup Leveler"
+      ],
+      answer: "Hyper Text Markup Language"
+    },
+    {
+      title: "Which symbol is used for single line comments in JavaScript?",
+      choices: [
+        "' '",
+        "//",
+        "/* */",
+        "<!-- -->"
+      ],
+      answer: "//"
+    },
+    {
+      title: "How do you create a function in JavaScript?",
+      choices: [
+        "function myFunction()",
+        "function = myFunction()",
+        "function:myFunction()",
+        "create myFunction()"
+      ],
+      answer: "function myFunction()"
+    },
+    {
+      title: "Which method would you use to round the number 7.25 to the nearest integer?",
+      choices: [
+        "Math.round(7.25)",
+        "Math.ceil(7.25)",
+        "Math.floor(7.25)",
+        "Math.random(7.25)"
+      ],
+      answer: "Math.round(7.25)"
+    },
+    {
+      title: "How do you declare a JavaScript variable?",
+      choices: [
+        "variable carName",
+        "var carName",
+        "v carName",
+        "let carName"
+      ],
+      answer: "var carName"
+    },
+    {
+      title: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+      choices: [
+        "<script name='xxx.js'>",
+        "<script href='xxx.js'>",
+        "<script src='xxx.js'>",
+        "<script file='xxx.js'>"
+      ],
+      answer: "<script src='xxx.js'>"
+    },
+    {
+      title: "How do you write 'Hello World' in an alert box?",
+      choices: [
+        "msg('Hello World');",
+        "alertBox('Hello World');",
+        "alert('Hello World');",
+        "msgBox('Hello World');"
+      ],
+      answer: "alert('Hello World');"
+    },
+    {
+      title: "How do you call a function named 'myFunction'?",
+      choices: [
+        "call myFunction()",
+        "call function myFunction()",
+        "myFunction()",
+        "call myFunction"
+      ],
+      answer: "myFunction()"
+    },
+    {
+      title: "How to write an IF statement in JavaScript?",
+      choices: [
+        "if i = 5 then",
+        "if i == 5 then",
+        "if (i == 5)",
+        "if i = 5"
+      ],
+      answer: "if (i == 5)"
+    },
+    {
+      title: "How can you add a comment in a JavaScript?",
+      choices: [
+        "'This is a comment",
+        "//This is a comment",
+        "<!--This is a comment-->",
+        "*This is a comment*"
+      ],
+      answer: "//This is a comment"
+    }
+  ];
+  
+  
+  
+  
+  
+  
